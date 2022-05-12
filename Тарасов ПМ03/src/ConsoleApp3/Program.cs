974c97ee0ae91f341da754fddd8feccfeae5a317
Slaksy <95918850+Slaksy@users.noreply.github.com>
2022-05-12 15:41:05 +0300

    Add files via upload

---
+++ Тарасов ПМ03/src/ConsoleApp3/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+namespace ConsoleApp3
+{
+    struct Smartphone
+    {
+        public string marka;
+
+        public string model;
+
+        public int price;
+
+    }
+    class DeviceShop
+    {
+        static void Main(string[] args)
+        {
+            Smartphone obj = new Smartphone();
+
+            int a, b = 0;
+
+            Console.Write("Введите размер массива = ");
+
+            while (!int.TryParse(Console.ReadLine(), out a))
+            {
+                Console.Write("Ошибка ввода! Введитее ещё раз = ");
+            }
+            object[] mas = new object[a * 4];
+
+            for (int i = 0; i < a; i++)
+            {
+                Console.Write("Введите марку = ");
+
+                obj.marka = Console.ReadLine();
+
+                mas[b] = obj.marka;
+
+                b++;
+
+                Console.Write("Введите модель = ");
+
+                obj.model = Console.ReadLine();
+
+                mas[b] = obj.model;
+
+                b++;
+
+                Console.Write("Введите цену = ");
+
+                while (!int.TryParse(Console.ReadLine(), out obj.price))
+                {
+
+                }
+                mas[b] = obj.price;
+                b++;
+
+            }
+            StreamWriter c = new StreamWriter("DeviceShop.txt");
+            b = 0;
+            for (int i = 0; i < a; i++)
+            {
+                c.WriteLine("Марка: " + mas[b]); b++;
+                c.WriteLine("Модель: " + mas[b]); b++;
+                c.WriteLine("Цена: " + mas[b]); b++;
+            }
+            c.Close();
+        }
+    }
+
+
+}
